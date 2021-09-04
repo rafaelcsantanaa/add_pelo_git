0d9477954b3e01b9ce78a90a38900d65ac5d50b2
rafael <rafaelcostasantana13@gmail.com>
2021-09-03 22:38:01 -0300

    melhores menus

---
+++ main.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+
+
+
+    printf("\n   ÚÄÄÄÄÄÄÄÄ Menu  Funcao ÄÄÄÄÄÄÄÄ¿\n");
+   printf("   ³                              ³\n");
+   printf("   ³ Û Retornar menu principal    ³\n");
+   printf("   ³                              ³\n");
+   printf("   ÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÙ");
+
+printf("\n\n\n\n");
+
+       printf("   ÚÄÄÄÄÄÄP o n t i l h i s m oÄÄÄ¿\n");
+      printf("   ³                              ³\n");
+      printf("   ³Tamanho   |  Preco            ³\n");
+      printf("   ³  grande  |   198             ³\n");
+      printf("   ³  pequena |    175            ³\n");
+      printf("   ³  media   |     98            ³\n");
+      printf("   ÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÙ\n");
+
+
+   printf("   %c-----------%c----------%c------------%c \n",201,200,188,187);
+      printf("   | P o n t i l h i s m o             |\n");
+      printf("   |-----------------------------------|\n");
+      printf("   | <B>  Listar                       |\n");
+      printf("   |-----------------------------------|\n");
+      printf("   | <C>  Excluir cadastro             |\n");
+      printf("   |-----------------------------------|\n");
+      printf("   | <D>  Alterar cadastro             |\n");
+      printf("   |-----------------------------------|\n");
+      printf("   | <S>  Sair                         |\n");
+      printf("   %c-----------------------------------%c",200,188);
+      printf("\n\n\n\n");
+
+
+    printf ("Bem vindo ao sistema de simulação de crédito\n\n\n");
+	printf ("           Menu principal\n");
+	printf ("**************************************** \n");
+	printf ("*     1 - Cadastro de clientes         * \n");
+	printf ("*     2 - Consulta de clientes         * \n");
+	printf ("*     3 - Empréstimo                   * \n");
+	printf ("*     4 - Excluir cadastro             * \n");
+	printf ("*     5 - Sair                         * \n");
+	printf ("**************************************** \n\n");
+	printf ("Selecione a opção desejada: ");
+
+
+printf("\n\n\n\n");
+
+	printf ("-------------------------------------------- \n");
+	printf ("| Lista de clientes cadastrados (Total: %d) | \n");
+	printf ("-------------------------------------------- \n");
+
+
+printf("\n\n\n\n");
+
+
+printf("+-----+--------+--------+-----+--------+--------+-----+--------+--------+\n");
+printf("|                    .::Tabela do seno e cosseno::.                     |\n");
+printf("+-----+--------+--------+-----+--------+--------+-----+--------+--------+\n");
+printf("| Ang |    sen |    cos | Ang |    sen |    cos | Ang |    sen |    cos |\n");
+printf("+-----+--------+--------+-----+--------+--------+-----+--------+--------+\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
